b9d1f83eedd36f47b61ec8d2a75a43f47f7a0d99
paulah <paula.h.36@gmail.com>
2015-04-11 19:49:24 -0400

    Fixed ice cream random dislocations

--- drag.js
+++ drag.js
@@ -37,7 +37,7 @@ function dragDiv(e, targ) {
 	console.log($(targ))
 	targ.style.left=coordX+e.clientX-offsetX+'px';
 	targ.style.top=coordY+e.clientY-offsetY+'px';
-	if(parseInt(targ.style.left) < -480 && parseInt(targ.style.top) > 240) {
+	if(parseInt(targ.style.left) < 468 && parseInt(targ.style.top) > 320) {
 		$(targ).fadeOut();
 	}
 	return false;
